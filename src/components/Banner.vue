c36c42f51a8a176dc35b537f9c17019ad6d4527c
David Long <david_long3@dell.com>
2019-10-26 17:36:46 +0800

    Added Service component

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,104 +1,104 @@
 <template>
-    <div id="banner">
-        <div id="banner-menu">
-            <ul id="ul_top_hypers">
-                <li class="menu-item"><a href="" class="a_top_hypers"> 主页</a></li>
-                <li class="menu-item"><a href="" class="a_top_hypers"> 关于</a></li>
-                <li class="menu-item"><a href="" class="a_top_hypers"> 服务</a></li>
-                <li class="menu-item"><a href="" class="a_top_hypers"> 作品</a></li>
-                <li class="menu-item"><a href="" class="a_top_hypers"> 联系我们</a></li>
-            </ul>
-        </div>
-        <b-carousel
-                id="carousel-1"
-                v-model="slide"
-                :interval="0"
-                controls
-                indicators
-                background="#ababab"
-                img-width="1024"
-                img-height="480"
-                style="text-shadow: 1px 1px 2px #333;"
-                @sliding-start="onSlideStart"
-                @sliding-end="onSlideEnd"
-        >
-            <!-- Text slides with image -->
-            <b-carousel-slide
-                    caption=""
-                    text=""
-                    img-src="../assets/banner/hoop.jpg"
-            ></b-carousel-slide>
+  <div id="banner">
+    <ul id="banner-menu">
+      <li class="menu-item">
+        <a href class="a_top_hypers">主页</a>
+      </li>
+      <li class="menu-item">
+        <a href class="a_top_hypers">关于</a>
+      </li>
+      <li class="menu-item">
+        <a href class="a_top_hypers">服务</a>
+      </li>
+      <li class="menu-item">
+        <a href class="a_top_hypers">作品</a>
+      </li>
+      <li class="menu-item">
+        <a href class="a_top_hypers">联系我们</a>
+      </li>
+    </ul>
 
-            <!-- Slides with custom text -->
-            <b-carousel-slide img-src="../assets/banner/subway.jpeg">
-                <h1>Hello world!</h1>
-            </b-carousel-slide>
+    <b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="0"
+      controls
+      indicators
+      background="#ababab"
+      img-width="1024"
+      img-height="480"
+      style="text-shadow: 1px 1px 2px #333;"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <!-- Text slides with image -->
+      <b-carousel-slide caption text img-src="../assets/banner/hoop.jpg"></b-carousel-slide>
 
-            <!-- Slides with img slot -->
-            <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-            <b-carousel-slide>
-                <template v-slot:img>
-                    <img
-                            class="d-block img-fluid w-100"
-                            width="1024"
-                            height="480"
-                            src="../assets/banner/mountain.jpg"
-                            alt="image slot"
-                    >
-                </template>
-            </b-carousel-slide>
-        </b-carousel>
+      <!-- Slides with custom text -->
+      <b-carousel-slide img-src="../assets/banner/subway.jpeg">
+        <h1>Hello world!</h1>
+      </b-carousel-slide>
 
-        <!-- <p class="mt-4">
+      <!-- Slides with img slot -->
+      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
+      <b-carousel-slide>
+        <template v-slot:img>
+          <img
+            class="d-block img-fluid w-100"
+            width="1024"
+            height="480"
+            src="../assets/banner/mountain.jpg"
+            alt="image slot"
+          />
+        </template>
+      </b-carousel-slide>
+    </b-carousel>
+
+    <!-- <p class="mt-4">
             Slide #: {{ slide }}<br>
             Sliding: {{ sliding }}
-        </p> -->
-    </div>
+    </p>-->
+  </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                slide: 0,
-                sliding: null
-            }
-        },
-        methods: {
-            onSlideStart() {
-                this.sliding = true
-            },
-            onSlideEnd() {
-                this.sliding = false
-            }
-        }
+export default {
+  data() {
+    return {
+      slide: 0,
+      sliding: null
+    };
+  },
+  methods: {
+    onSlideStart() {
+      this.sliding = true;
+    },
+    onSlideEnd() {
+      this.sliding = false;
     }
+  }
+};
 </script>
 
 <style scoped>
-    #banner {
-        position: relative;
-    }
-    #banner-menu {
-        width: 100%;
-        z-index: 100;
-        position: absolute;
-        top: 10%;
-        left: 10%;
-    }
-    #div_top_hypers {
-        #background-color:#eeeeee;
-        #display:inline;
-    }
-    #ul_top_hypers li{
-        #display: inline;
-    }
-    .a_top_hypers {
-        color: white;
-        font-size: small;
-    }
-    .menu-item {
-        margin: 0 3%;
-        display: inline-block;
-    }
+#banner {
+  position: relative;
+}
+#banner-menu {
+  width: 100%;
+  z-index: 100;
+  position: absolute;
+  top: 10%;
+  right: 10%;
+  text-align: right;
+}
+
+.a_top_hypers {
+  color: white;
+  font-size: small;
+}
+.menu-item {
+  margin: 0 3%;
+  display: inline-block;
+}
 </style>
---
+++ src/components/Services.vue
@@ -0,0 +1,125 @@
+<template>
+  <section>
+    <h2 class="header mb-4" v-scroll-reveal>我们提供如下服务</h2>
+    <ul class="service-container">
+      <li
+        class="service"
+        v-for="(service,index) in services"
+        :key="index"
+        v-scroll-reveal="{delay: index*100}"
+      >
+        <div class="service-box">
+          <img :src="service.imageName" width="100" :alt="service.title" />
+          <h2>{{service.title}}</h2>
+          <p>{{service.content}}</p>
+        </div>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+const services = [
+  {
+    imageName: "website.svg",
+    title: "传统网站/软件开发",
+    content:
+      "我们精通 js, java, python, c# 等多种开发语言，能够胜任传统网站/软件开发任务。"
+  },
+  {
+    imageName: "mobile.svg",
+    title: "移动端开发",
+    content:
+      "我们拥有设计，开发移动端App和微信小程序的经验，让您的业务“动”起来。"
+  },
+  {
+    imageName: "bigdata.svg",
+    title: "大数据",
+    content:
+      "我们可以帮助您收集，整理，提取大数据，并指导您如何利用大数据来提高您的业务和决策。"
+  },
+  {
+    imageName: "ai.svg",
+    title: "人工智能",
+    content:
+      "通过建立机器学习模型，我们让您构建更高层的应用，比如推荐系统，预测系统等。"
+  },
+  {
+    imageName: "cloud-service.svg",
+    title: "云服务",
+    content:
+      "我们通过云服务，让您的企业的系统包括app，数据库，机器学习的模型等移到云端，从而提高效率，节省开支，降低成本，并提高灵活性。"
+  },
+  {
+    imageName: "design.svg",
+    title: "体验设计",
+    content:
+      "我们始终相信以人为本的思路将会让您的产品及服务脱颖而出。结合硅谷最前沿的设计思维，我们指导您给客户提供优异的用户体验。"
+  },
+  {
+    imageName: "brand.svg",
+    title: "品牌战略",
+    content:
+      "品牌，和产品的质量一样重要。我们通过数字化祝您完成品牌升级，通过品牌的构建，让您的产品独一无二。"
+  },
+  {
+    imageName: "user-insight.svg",
+    title: "用户洞察",
+    content:
+      "我们提供最前沿的定性及定量分析技术，辅助您洞察您的用户，清晰的了解用户的痛点以及产品的改进方案。"
+  }
+];
+
+export default {
+  name: "Services",
+  data: () => ({
+    services: services.map(service => ({
+      ...service,
+      imageName: require("../assets/" + service.imageName)
+    }))
+  })
+};
+</script>
+
+<style scoped>
+.header {
+  font-size: 2rem;
+}
+
+.service-container {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: -1rem;
+}
+
+.service {
+  display: flex;
+  width: 25%;
+  padding: 0 1rem;
+  visibility: hidden;
+}
+
+.service-box {
+  width: 100%;
+  margin-top: 1rem;
+  padding: 0 1rem;
+  background: #f5f5f5;
+  transition: all 0.75s ease 0s;
+  transform: translateX(0px) translateY(0px);
+}
+
+.service:nth-child(odd) .service-box:hover {
+  background: #e3e1d3;
+}
+
+.service:nth-child(even) .service-box:hover {
+  background: #e0cece;
+}
+
+@media (max-width: 767.98px) {
+  .service {
+    width: 100%;
+    padding: 0 0.5rem;
+  }
+}
+</style>
